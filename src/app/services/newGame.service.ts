a1548aa3872c656a5f769de1cd00388f5508e7e4
JessicaFresnedaH <jessyinad@gmail.com>
2018-02-23 16:26:14 -0500

    Juegos top 5 , en lista con descripcion, falta foto

--- src/app/services/newGame.service.ts
+++ src/app/services/newGame.service.ts
@@ -8,10 +8,11 @@ import 'rxjs/add/observable/of';
 @Injectable()
 export class NewGameService {
    private newGames: NewGame[]=[
-     new NewGame('Need for Speed',' It is a franchise of video game of racing action created by Electronic Arts and currently developed by Ghost Games . ',"https://images.sftcdn.net/images/t_optimized,f_auto/p/5c8d74c2-9b24-11e6-82eb-00163ec9f5fa/1451573144/need-for-speed-carbono-screenshot.jpg"),
-     new NewGame('FIFA17','eSTE ES UN JUEGO DE FOOTBOOL2','NULL'),
-     new NewGame('FIFA11','eSTE ES UN JUEGO DE FOOTBOOL3','NULL')
-
+     new NewGame(' ','Need for Speed',' It is a franchise of video game of racing action created by Electronic Arts and currently developed by Ghost Games.'),
+     new NewGame('NULL','Warcraft: Orcs & Humans','Is a strategy video game created by Blizzard Entertainment for PC. It is one of the first games of the real-time strategy or RTS genre . Warcraft: Orcs & Humans starts the Warcraft series and takes place in a medieval epic environment where humans from the mythical kingdom of Azeroth confront the invading orcs among other fantastic creatures .'),
+     new NewGame('NULL','League of Legends','Is a competitive online game with a frenetic rhythm that fuses the speed and intensity of the real-time strategy (ETR) with elements of role-playing games. Two teams of powerful champions, one with a unique design and style of games, compete with the face through various battlefields and game modes. With an ever-expanding roster of champions, frequent updates and an exciting competitive landscape, League of Legends offers unlimited gaming possibilities for all skill levels.'),
+     new NewGame(' ','Sea of Thieves',' Sea of ​​Thieves is a first-person video game developed by Rare and distributed by Microsoft. It will go on sale in 2018 for Xbox One'),
+     new NewGame(' ','Super Mario Bros.','Is a platform video game, designed by Shigeru Miyamoto, released on September 13, 1985 and produced by the company Nintendo, for the Nintendo Entertainment System (NES). The game describes the adventures of brothers Mario and Luigi, characters who already starred in the arcade Mario Bros. of 1983. This time both must rescue Princess Peach Mushroom Kingdom who was kidnapped by the King of the Koopas, Bowser.')
    ];
     constructor(){}
 
@@ -22,7 +23,4 @@ export class NewGameService {
         this.newGames.push(new NewGame(name,description,photo))
 
       }
-      
-
-
 }
