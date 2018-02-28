db97e2f100c533760f48162b67c4919dcea37588
carolMeneses <carol.meneses@mail.escuelaing.edu.co>
2018-02-28 15:45:08 -0500

    uniendo comentarios

--- src/app/services/descriptionGame.service.ts
+++ src/app/services/descriptionGame.service.ts
@@ -11,7 +11,7 @@ import 'rxjs/add/observable/of';
 export class DescriptionGameService {
    private DescriptionGames: DescriptionGame[]=[
      new DescriptionGame('https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros_image1600w.jpg','Mario Bros','Diseñada bajo la premisa de una buena relación calidad precio, Nintendo 2DS es perfecta como primera consola y permite jugar los títulos más populares de Nintendo 3DS en 2D. Con Nintendo 2DS podrás disfrutar de juegos, aplicaciones y funciones increíbles que te permitirán conectar con amigos y con la comunidad global de Nintendo.','$359.900 ','GameStory','www.Nitendo.com'),
-     new DescriptionGame('assets/images/mario.jpg','Mario Bros','The announcement sent Nintendo d expect. But fans hoping for iOS versions of early Super Mario Bros and Zelda games, or even ports of N64-era classics, should park those expectations, as Ninty will be working on new games from the ground up, tailored to pocket-sized touchscreen devices','$500 000 ','TiendaOnline','https://listado.mercadolibre.com.mx/juegos-de-mario-bros-gratis'),
+     new DescriptionGame('assets/images/mario.jpg','Mario Bros','The announcement sent Nintendo d expect. But fans hoping for iOS versions of early Super Mario Bros and Zelda games, or even ports of N64-era classics, should park those expectations, as Ninty will be working on new games from the ground up, tailored to pocket-sized touchscreen devices','$500 000 ','TiendaOnline','listado.mercadolibre.com.mx/juegos-de-mario-bros-gratis'),
      
      
     
